2357d5cca026a8653939261a2bbd1e189529b713
MoroianuAndrei <madalin.moroianu@student.unitbv.ro>
2025-04-04 11:42:16 +0300

    Change SignInView

---
+++ MemoryGame/Commands/NullToVisibilityConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace MemoryGame.Commands
+{
+    public class NullToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool invert = parameter != null && parameter.ToString().ToLower() == "true";
+            bool isNull = value == null;
+
+            if (invert)
+                return isNull ? Visibility.Visible : Visibility.Collapsed;
+            else
+                return isNull ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
